716a25d4497493aa6614c4ec60783403d70fe67d
anandu9805 <anandu0598@gmail.com>
2025-02-01 20:24:46 +0530

    bearer token hidden

--- src/utils.js/constant.js
+++ src/utils.js/constant.js
@@ -3,11 +3,3 @@ export const loginPageBg =
   "https://assets.nflxext.com/ffe/siteui/vlv3/154a9550-ce07-4e28-819c-63185dd849f8/web/IN-en-20250106-TRIFECTA-perspective_27b02e7c-f668-4639-9e82-1a5485084b2a_small.jpg";
 export const netflixLogo =
   "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
-export const API_OPTIONS = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Nzg3NzkxYjNiNmE2OTJkMmUwNWE3MDUyNjExZDk3NyIsIm5iZiI6MTczODM4NTI5NS4wMTQwMDAyLCJzdWIiOiI2NzlkYTc4ZmFjNWE3OTUxYjljYjM3NDkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Hg_Nl4fgWzcyDzvcpDu4lVSjzGPT-SeJRxHGbrLfvOU",
-  },
-};
