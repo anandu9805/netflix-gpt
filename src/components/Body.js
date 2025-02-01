ebc5dea0a63a22a0a69a528a78486b765500dca4
anandu9805 <anandu0598@gmail.com>
2025-02-01 18:32:18 +0530

    vedio background and vedio title

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,28 +1,11 @@
 import Login from "./Login";
 import Browse from "./Browse";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../utils.js/firebase";
-import { useDispatch } from "react-redux";
-import { addUser, removeUser } from "../utils.js/appStore/userSlice";
 const Body = () => {
-  const dispatch = useDispatch();
   const appRouter = createBrowserRouter([
     { path: "/", element: <Login /> },
     { path: "/browse", element: <Browse /> },
   ]);
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        const { uid, email, displayName } = user;
-        dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
-      } else {
-        dispatch(removeUser());
-      }
-    });
-  }, []);
 
   return (
     <div>
